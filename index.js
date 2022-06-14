b5f454de89a3a078c1e124beb526a7b71e71f004
Dustin4d <dustinholder14@yahoo.com>
2022-06-13 19:38:19 -0500

    Add player1 and player2 choice randomization

---
+++ index.js
@@ -0,0 +1,16 @@
+// Main function
+const computerPlay = () => {
+    // Show all possible choices
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    
+    // player1 random choice
+    const player1 = choices[Math.floor(Math.random() * 3)];
+    //player2 random choice
+    const player2 = choices[Math.floor(Math.random() * 3)];
+    //console.log("Choose by inputting R, P, or S. Then press enter.");
+
+    // Determine winner
+
+}
+
+computerPlay();
