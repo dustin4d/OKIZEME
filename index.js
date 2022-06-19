0fb7721ca369dd755fb357726b4d4c802246a5e0
Dustin4d <dustinholder14@yahoo.com>
2022-06-19 16:50:09 -0500

    Add win/loss logic for player1 and player2

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let player1Score = 0;
+let player2Score = 0;
+
 // Main function
 const computerPlay = () => {
     // Show all possible choices
@@ -9,8 +12,26 @@ const computerPlay = () => {
     const player2 = choices[Math.floor(Math.random() * 3)];
     //console.log("Choose by inputting R, P, or S. Then press enter.");
 
-    // Determine winner
-
+    // Determine winner and add points to player's score.
+    if(player1 === player2){
+        console.log("Tie match, go again.");
+    }
+    else if (
+        (player1 == 'Rock') && (player2 == 'Scissors') ||
+        (player1 == 'Scissors') && (player2 == 'Paper') ||
+        (player1 == 'Paper') && (player2 == 'Rock')
+    ) {
+        player1Score++;
+        console.log("Player 1 wins.")
+    }
+    else if (
+        (player2 == 'Rock') && (player1 == 'Scissors') ||
+        (player2 == 'Scissors') && (player1 == 'Paper') ||
+        (player2 == 'Paper') && (player1 == 'Rock')
+    ) {
+        player2Score++;
+        console.log("Player 2 wins.");
+    }
 }
 
 computerPlay();
